0d3f61a502dafbbd03fcb750c30bad542bb55953
zubikarai <7schets7@gmail.com>
2022-03-14 01:45:36 +0500

    initial commit

---
+++ lib/game.rb
@@ -0,0 +1,69 @@
+class Game
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def letters_to_guess
+    result =
+      @letters.map do |letter|
+        if @user_guesses.include?(normalize_letter(letter))
+        letter
+        else
+        nil
+        end
+      end
+    result
+  end
+
+  def lost?
+    errors_allowed == 0
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    if !over? && !@user_guesses.include?(normalize_letter(letter))
+      @user_guesses << normalize_letter(letter)
+    end
+  end
+
+  def word
+    @letters.join
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def normalize_letter(letter)
+    case letter
+    when "Ё"
+      "Е"
+    when "Й"
+      "И"
+    else
+      letter
+    end
+  end
+
+  def normalized_letters
+    @letters.map { |letter| normalize_letter(letter) }
+  end
+end
